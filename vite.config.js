b72a0e2f438f66d49ea105bc0a5fa1c3ff1b5424
RobyDoby <jettann94@gmail.com>
2022-08-11 19:47:23 +0300

    added features section

--- vite.config.js
+++ vite.config.js
@@ -69,7 +69,7 @@ export default defineConfig({
                },
                {
                   name: 'removeEmptyAttrs',
-                  active: false,
+                  active: true,
                },
             ],
          },
