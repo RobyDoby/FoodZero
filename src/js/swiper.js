c89e60ed15892395a2f6a5bd143eaf301d08ab9c
RobyDoby <jettann94@gmail.com>
2022-08-13 00:43:26 +0300

    added testimonials swiper

---
+++ src/js/swiper.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+   speed: 400,
+   spaceBetween: 100,
+
+   // If we need pagination
+   pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+   },
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+});
